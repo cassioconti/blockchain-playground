7772e6a7043e3798e2e3e5d00485c7b1ada00576
Cassio Conti <cassiorconti@gmail.com>
2018-07-22 17:25:03 -0500

    Add post block endpoint

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
+const bodyParser = require("body-parser");
 const RequestHandler = require('./request-handler');
+
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 const requestHandler = new RequestHandler();
 
-app.get('/block/:height', requestHandler.getBlock);
-app.post('/', requestHandler.postBlock);
+app.get('/block/:height', (req, res) => requestHandler.getBlock(req, res));
+app.post('/block', (req, res) => requestHandler.postBlock(req, res));
 
 const port = 8000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
--- request-handler.js
+++ request-handler.js
@@ -1,15 +1,31 @@
 const Blockchain = require('./blockchain');
+const Block = require('./block');
 
 class RequestHandler {
     getBlock(req, res) {
-        Blockchain.getInstance()
-            .then(instance => instance.getBlock(req.params.height))
-            .then(block => res.json(block))
-            .catch(err => res.send(err));
+        this.getBlockCore(req.params.height, res);
     }
 
     postBlock(req, res) {
-        res.send('Hello world POST');
+        if (req.body.data) {
+            const newBlock = new Block(req.body.data);
+            Blockchain.getInstance()
+                .then(instance => instance.addBlock(newBlock))
+                .then(instance => instance.getBlockHeight())
+                .then(height => this.getBlockCore(height - 1, res));
+        } else {
+            res.status(400).json({
+                reason: 'Bad request.',
+                details: 'Expected json containing "data" attribute.'
+            });
+        }
+    }
+
+    getBlockCore(height, res) {
+        Blockchain.getInstance()
+            .then(instance => instance.getBlock(height))
+            .then(block => res.json(block))
+            .catch(err => res.send(err));
     }
 }
 
